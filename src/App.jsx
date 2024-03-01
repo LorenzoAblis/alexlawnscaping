99fe97d433e02f8573eceec885fbf35ba0b3f2c3
lorenzoablis <108164703+LorenzoAblis@users.noreply.github.com>
2024-03-01 03:59:46 +0000

    feat: Added bootstrap-icons package version 1.11.3, adjusted styles in Home component for better alignment and appearance, introduced new images 'lawn.jpg' and 'passion.jpg' in Home component and updated background color and text color

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./Home";
 import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/shopping" replace />} /> */}
-        <Route path="/home" element={<Home />} />
-        {/* <Route path="/inventory" element={<Inventory />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
