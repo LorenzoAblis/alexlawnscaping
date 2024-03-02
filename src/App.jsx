0337d7458360d0c274e23c36c2a9aea09cdb004c
lorenzoablis <108164703+LorenzoAblis@users.noreply.github.com>
2024-03-02 17:23:16 +0000

    asdasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./Home";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return <Home />;
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "./assets/logo.png";
 import lawn from "./assets/lawn.jpg";
 import passion from "./assets/passion.jpg";
+import mowing from "./assets/mowing.jpg";
+import edging from "./assets/edging.png";
+import fertilization from "./assets/fertilization.jpg";
 import "./Home.scss";
 
 const Home = () => {
@@ -10,9 +13,9 @@ const Home = () => {
       <nav className="d-flex justify-content-between p-3 shadow">
         <img src={logo} alt="logo" />
         <div className="d-flex gap-5">
-          <button>About</button>
-          <button>Pricing</button>
-          <button>FAQ</button>
+          <a href="#about-section">About</a>
+          <a href="#services-section">Services</a>
+          <a href="#location-section">Location</a>
           <button className="contact">
             <i className="bi bi-telephone-fill mr-3"></i> 312-444-1423
           </button>
@@ -23,8 +26,8 @@ const Home = () => {
         <h1>Where Lawns Thrive, We Arrive.</h1>
       </section>
 
-      <section className="passion-section">
-        <img src={passion} alt="" />
+      <section className="passion-section" id="about-section">
+        <img src={passion} alt="" className="shadow-lg" />
 
         <section className="passion-content d-flex">
           <div className="vertical" />
@@ -46,6 +49,95 @@ const Home = () => {
             </p>
           </article>
         </section>
+      </section>
+
+      <section className="services-section" id="services-section">
+        <div className="services-header">
+          <h1 className="fw-bold services-title">Our Services</h1>
+          <h5 className="services-subtitle">
+            We provide a comprehensive range of services in the Lawn Care
+            industry, ensuring your lawn stays healthy and beautiful all year
+            round.
+          </h5>
+        </div>
+        <div className="services-content d-flex">
+          <article className="card">
+            <img src={mowing} alt="" className="shadow" />
+            <h3 className="fw-bold card-title">Mowing</h3>
+            <p className="card-text">
+              Our mowing service ensures your lawn can breathe, promoting root
+              growth and improving water and nutrient absorption. Let us help
+              your lawn thrive with our profession
+            </p>
+          </article>
+          <article className="card">
+            <img src={edging} alt="" className="shadow" />
+            <h3 className="fw-bold card-title">Edging</h3>
+            <p className="card-text">
+              Edging gives your lawn a clean, manicured look. Our experts create
+              clear lines between your lawn and adjacent areas, enhancing the
+              overall aesthetic of your outdoor space.
+            </p>
+          </article>
+          <article className="card">
+            <img src={fertilization} alt="" className="shadow" />
+            <h3 className="fw-bold card-title">Fertilization</h3>
+            <p className="card-text">
+              Fertilizing is key to a lush, green lawn. We provide a balanced
+              blend of nutrients tailored to your lawn's specific needs,
+              promoting healthy growth and resilience.
+            </p>
+          </article>
+        </div>
+      </section>
+
+      <section className="testimonials-section" id="testimonials-section">
+        <div className="testimonials-header">
+          <h4 className="testimonials-subtitle fw-semibold">Testimonals</h4>
+          <h2 className="testimonials-title fw-semibold">
+            Voices of Satisfied Customers
+          </h2>
+        </div>
+        <div className="testimonials-content d-flex">
+          <article className="card">
+            <p className="testimonials-text">
+              Exceptional lawn care service! My yard has never looked better.
+            </p>
+            <h4 className="testimonials-name">- Joe G</h4>
+          </article>
+          <article className="card">
+            <p className="testimonials-text">
+              Top-notch service! They transformed my lawn beautifully.
+            </p>
+            <h4 className="testimonials-name">- Love Q</h4>
+          </article>
+          <article className="card">
+            <p className="testimonials-text">
+              Highly recommend! Their weed control is the best.
+            </p>
+            <h4 className="testimonials-name">- Guvienvere B</h4>
+          </article>
+        </div>
+      </section>
+
+      <section className="location-section" id="location-section">
+        <iframe
+          loading="lazy"
+          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBsh6yJba2R7lA-Varqc1qL9--6Xgmdi_w&q=Mount Prospect, IL"
+          className="shadow"
+        ></iframe>
+        <div className="location-content">
+          <div>
+            <h1 className="fw-bold">Find us Here</h1>
+            <p className="location-text">
+              Our Lawn Care company is conveniently located at your service. You
+              can find us on the map provided below. We are your local experts
+              in Aeration, Edging, Fertilizing, Mowing, Trimming, and Weed
+              Control. Don't hesitate to visit us for all your lawn care needs.
+            </p>
+          </div>
+          <div></div>
+        </div>
       </section>
 
       <footer className="footer bg-black p-3">
