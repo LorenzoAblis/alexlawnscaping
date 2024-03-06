fecdf5901a14c0b0c2774c21e7e5b5bf328c8607
lorenzoablis <108164703+LorenzoAblis@users.noreply.github.com>
2024-03-06 16:52:55 +0000

    feat: Add sticky navbar functionality with transition and animation effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import "../styles/Navbar.scss";
 import logoTransparent from "../assets/logoTransparent.png";
 
 const Navbar = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 100);
+    });
+  });
   return (
     <header>
-      <nav className="d-flex justify-content-between p-3 shadow">
+      <nav
+        className={`d-flex justify-content-between p-3 ${scroll ? "sticky" : ""}`}
+      >
         <img src={logoTransparent} alt="logo" />
         <div className="d-flex gap-5">
           <a href="#about-section">About</a>
