44923ebae28a834f563fe94c5dd63078accfa9c9
EnzoAblis <112990234+EnzoAblis@users.noreply.github.com>
2025-03-16 11:19:16 -0500

    before/after section responsivity

--- src/components/BeforeAfter.jsx
+++ src/components/BeforeAfter.jsx
@@ -29,12 +29,13 @@ const BeforeAfter = () => {
         <h4 className="text-2xl font-bold text-secondary">Our Work</h4>
         <h2 className="text-3xl mt-5 font-bold text-primary">Lawn Cleanups</h2>
       </div>
-      <div className="mt-5 flex flex-row w-6/7 overflow-x-scroll snap-x snap-mandatory gap-5 text-center h-80 pr-5">
-        {collages.map((collage, index) => (
+      <div className="mt-5 flex flex-row w-6/7 overflow-x-scroll snap-x snap-mandatory gap-5 text-center h-80 pr-5 md:h-full md:gap-10 pb-5 lg:overflow-x-visible lg:flex-wrap">
+        {collages.map((collage) => (
           <img
             src={`/beforeAfter/${collage}.webp`}
             alt={collage}
-            className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center"
+            className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center md:w-1/2 lg:w-1/4"
+            loading="lazy"
           />
         ))}
       </div>
@@ -42,18 +43,23 @@ const BeforeAfter = () => {
         <h4 className="text-2xl font-bold text-secondary">Our Work</h4>
         <h2 className="text-3xl mt-5 font-bold text-primary">Before & After</h2>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center lg:w-full">
         {photoPairs.map((pair, index) => (
-          <div className="mt-5 flex flex-row w-6/7 overflow-x-scroll snap-x snap-mandatory gap-5 text-center h-80 pr-5">
+          <div
+            className="mt-5 flex flex-row w-6/7 overflow-x-scroll snap-x snap-mandatory gap-5 text-center h-80 pr-5 nth-4:h-full pb-5 md:nth-4:h-100 lg:overflow-x-visible"
+            key={index}
+          >
             <img
               src={`/beforeAfter/${pair.before}.webp`}
               alt={pair.before}
-              className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center h-7/8"
+              className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center h-7/8 lg:w-1/3"
+              loading="lazy"
             />
             <img
               src={`/beforeAfter/${pair.after}.webp`}
               alt={pair.after}
-              className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center h-7/8"
+              className="object-cover w-6/7 rounded-4xl m-auto shadow-[0.5rem_0.5rem_0rem_var(--color-secondary)] snap-center h-7/8 lg:w-1/3"
+              loading="lazy"
             />
           </div>
         ))}
